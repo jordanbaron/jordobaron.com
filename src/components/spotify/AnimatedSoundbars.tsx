a22e36b3e444a6ae8feff45a6adb96b834fc1a59
Jordan Baron <jordannbaron@gmail.com>
2025-11-02 15:25:14 -0500

    yerrr

--- src/components/spotify/AnimatedSoundbars.tsx
+++
@@ -1,9 +0,0 @@
-export function AnimatedSoundbars() {
-  return (
-    <div class="flex gap-2 w-max h-8 rotate-180 mr-2 *:bg-white :*:h-[3px] *:h-[3px] *:w-[3px]">
-      <div class="bar"></div>
-      <div class="bar"></div>
-      <div class="bar"></div>
-    </div>
-  );
-}
