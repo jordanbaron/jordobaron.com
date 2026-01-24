26a827611aacf6ce076e27b811642498e5029980
Jordan Baron <jordannbaron@gmail.com>
2026-01-23 23:06:32 -0500

    switch to using pocketbase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 import solidJs from "@astrojs/solid-js";
 
@@ -12,6 +12,6 @@ export default defineConfig({
       theme: "monokai",
     },
   },
-  output: "server",
+  output: "static",
   adapter: vercel(),
 });
